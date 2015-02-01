c6a4cba7320175e3a67e9829da1c9ceb639276d3
Douglas Christopher Wilson <doug@somethingdoug.com>
2015-02-01 18:44:44 -0500

    build: remove request dependency

--- scripts/update.js
+++ scripts/update.js
@@ -1,12 +1,14 @@
 
 var fs = require('fs')
-var request = require('request')
+var http = require('http')
 var parser = require('csv-parse')
 var toArray = require('stream-to-array')
 
 var URL = 'http://www.iana.org/assignments/http-status-codes/http-status-codes-1.csv'
 
-toArray(request(URL).pipe(parser()), function (err, codes) {
-  if (err) throw err
-  fs.writeFile('src/iana.json', JSON.stringify(codes))
+http.get(URL, function onResponse(res) {
+  toArray(res.pipe(parser()), function (err, codes) {
+    if (err) throw err
+    fs.writeFile('src/iana.json', JSON.stringify(codes))
+  })
 })
