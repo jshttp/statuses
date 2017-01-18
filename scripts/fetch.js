085a0561875b9bfc837eced0a48f6ca09534624f
Douglas Christopher Wilson <doug@somethingdoug.com>
2017-01-18 00:19:44 -0500

    build: fetch over https

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -1,12 +1,12 @@
 
 var fs = require('fs')
-var http = require('http')
+var https = require('https')
 var parser = require('csv-parse')
 var toArray = require('stream-to-array')
 
-var URL = 'http://www.iana.org/assignments/http-status-codes/http-status-codes-1.csv'
+var URL = 'https://www.iana.org/assignments/http-status-codes/http-status-codes-1.csv'
 
-http.get(URL, function onResponse (res) {
+https.get(URL, function onResponse (res) {
   toArray(res.pipe(parser()), function (err, codes) {
     if (err) throw err
     fs.writeFile('src/iana.json', JSON.stringify(codes))
