9a64ee901c58e043153e0dc250fe3dbe1356954a
Douglas Christopher Wilson <doug@somethingdoug.com>
2017-10-19 23:05:02 -0400

    build: use static require in build script

--- scripts/build.js
+++ scripts/build.js
@@ -3,21 +3,32 @@
 var path = require('path')
 var write = require('./lib/write')
 
-// collect all the iana codes
-var codes = require('../src/iana.json')
+// all codes
+var codes = {}
 
-;[
-  'rfc',
-  'draft',
-  'apache',
-  'nginx',
-  'node'
-].forEach(function (src) {
-  var json = require('../lib/' + src + '.json')
-  Object.keys(json).forEach(function (key) {
-    codes[key] = json[key]
-  })
-})
+// initialize with all IANA codes
+addData(codes, require('../src/iana.json'))
+
+// add the codes from nginx
+addData(codes, require('../lib/nginx.json'))
+
+// add the codes from apache
+addData(codes, require('../lib/apache.json'))
+
+// add the codes from node
+addData(codes, require('../lib/node.json'))
+
+// add the rfc codes
+addData(codes, require('../lib/rfc.json'))
+
+// add the draft codes
+addData(codes, require('../lib/draft.json'))
 
 // write the JSON object
 write(path.join(__dirname, '../codes.json'), codes)
+
+function addData (db, obj) {
+  Object.keys(obj).forEach(function (key) {
+    db[key] = db[key] || obj[key]
+  })
+}
