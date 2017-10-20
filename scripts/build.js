d847b1c825e528d6e91153146e2f6d1efc124636
Douglas Christopher Wilson <doug@somethingdoug.com>
2017-10-20 13:47:56 -0400

    build: directly fetch apache codes

--- scripts/build.js
+++ scripts/build.js
@@ -9,14 +9,14 @@ var codes = {}
 // initialize with all IANA codes
 addData(codes, require('../src/iana.json'))
 
+// add the codes from node
+addData(codes, require('../src/node.json'))
+
 // add the codes from nginx
 addData(codes, require('../src/nginx.json'))
 
 // add the codes from apache
-addData(codes, require('../lib/apache.json'))
-
-// add the codes from node
-addData(codes, require('../src/node.json'))
+addData(codes, require('../src/apache.json'))
 
 // write the JSON object
 write(path.join(__dirname, '../codes.json'), codes)
---
+++ scripts/fetch-apache.js
@@ -0,0 +1,25 @@
+'use strict'
+
+var getBody = require('raw-body')
+var https = require('https')
+var path = require('path')
+var write = require('./lib/write')
+
+var URL = 'https://svn.apache.org/repos/asf/httpd/httpd/trunk/modules/http/http_protocol.c'
+
+https.get(URL, function onResponse (res) {
+  getBody(res, true, function (err, body) {
+    if (err) throw err
+
+    var block = /status_lines\[[^\]]*\]\s*=\s*{([^}]+)};/m.exec(body)[1]
+    var codes = {}
+    var match
+    var regexp = /"([0-9]+) ([^"]+)"/g
+
+    while ((match = regexp.exec(block))) {
+      codes[match[1]] = match[2]
+    }
+
+    write(path.join(__dirname, '../src/apache.json'), codes)
+  })
+})
